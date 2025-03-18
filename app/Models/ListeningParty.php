13666e7f1bd042fe91c20b3b2158d812304f28b4
Omar <omareegab@gmail.com>
2025-03-17 19:09:32 -0500

    format on save

--- app/Models/ListeningParty.php
+++ app/Models/ListeningParty.php
@@ -4,9 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/** @package App\Models */
 class ListeningParty extends Model
 {
     /** @use HasFactory<\Database\Factories\ListeningPartyFactory> */
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    /** @return BelongsTo  */
+    public function episodes(): BelongsTo
+    {
+        return $this->belongsTo(Episode::class);
+    }
 }
--- app/Models/Podcast.php
+++ app/Models/Podcast.php
@@ -4,9 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/** @package App\Models */
 class Podcast extends Model
 {
     /** @use HasFactory<\Database\Factories\PodcastFactory> */
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    /** @return HasMany  */
+    public function episodes(): HasMany
+    {
+        return $this->hasMany(Episode::class);
+    }
+
+    /** @return HasMany  */
+    public function listeningParties(): HasMany
+    {
+        return $this->hasMany(ListeningParty::class);
+    }
 }
