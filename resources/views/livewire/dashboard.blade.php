ffb4067280d07bccb73c6ab410e3cc889c31a747
Omar <omareegab@gmail.com>
2025-03-18 23:17:42 -0500

    add notes || work on create listening party

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,13 +1,33 @@
 <?php
 
+use Livewire\Attributes\Validate;
 use Livewire\Volt\Component;
 use App\Models\ListeningParty;
 
 new class extends Component {
+    #[Validate('required|string|max:255')]
     public string $name = '';
+
+    #[Validate('required')]
     public $startTime;
 
-    public function createListeningParty() {}
+    #[Validate('required|url')]
+    public string $mediaUrl = '';
+
+    public function createListeningParty()
+    {
+        $this->validate();
+
+        $episode = Episode::create([
+            'media_url' => $this->mediaUrl,
+        ]);
+
+        $listeningParty = ListeningParty::create([
+            'episode_id' => $episode->id,
+            'name' => $this->name,
+            'start_time' => $this->startTime,
+        ]);
+    }
 
     public function with()
     {
@@ -21,8 +41,9 @@ new class extends Component {
     <div class="max-w-lg w-full px-4">
         <form wire:submit='createListeningParty' class='space-y-3'>
             <x-input wire:model='name' placeholder="Listening party name" />
+            <x-input wire:model='mediaUrl' placeholder='URL podcast' description='RSS Feeds will grab the latest' />
             <x-datetime-picker wire:model='startTime' placeholder="Listening startTime" />
-            <x-button primary style="color: white;"> Create </x-button>
+            <x-button type="submit" style="color: white; font-weight:600;"> Create </x-button>
         </form>
     </div>
 </div>
