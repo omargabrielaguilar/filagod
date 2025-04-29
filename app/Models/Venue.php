d88b1ed3e7f03bfa06c803c876a143121169964c
Omar <saucyfunnel@gmail.com>
2025-04-29 11:08:06 -0500

    lazy vim for my workspace

--- app/Models/Venue.php
+++ app/Models/Venue.php
@@ -7,10 +7,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/** @package App\Models */
 class Venue extends Model
 {
     use HasFactory;
 
+    /** @return array  */
     protected function casts(): array
     {
         return [
