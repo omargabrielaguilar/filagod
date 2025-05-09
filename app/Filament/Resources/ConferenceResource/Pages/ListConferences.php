58c1cd36e1ccd169e6f994098f35cf6a7ba8fd71
Omar <saucyfunnel@gmail.com>
2025-05-09 09:10:29 -0500

    testing on vscode
    test

--- app/Filament/Resources/ConferenceResource/Pages/ListConferences.php
+++ app/Filament/Resources/ConferenceResource/Pages/ListConferences.php
@@ -5,11 +5,16 @@ namespace App\Filament\Resources\ConferenceResource\Pages;
 use App\Filament\Resources\ConferenceResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Contracts\Container\BindingResolutionException;
 
 class ListConferences extends ListRecords
 {
     protected static string $resource = ConferenceResource::class;
 
+    /**
+     * @return array<string|int, \Filament\Actions\Action|\Filament\Actions\ActionGroup> 
+     * @throws BindingResolutionException 
+     */
     protected function getHeaderActions(): array
     {
         return [
